b2996ae57e8d147a2d0ba0b83a3482e3d35e2f50
thoclua <95729422+thoclua@users.noreply.github.com>
2023-08-09 17:18:54 +0700

    commit code repository

---
+++ Data/Migrations/20230809101418_Updatedb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlantNestApp.Data.Migrations
+{
+    public partial class Updatedb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
