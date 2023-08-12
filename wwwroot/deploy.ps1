b4c754b5ef40cab755705b5ec78c5303028d0232
thoclua <95729422+thoclua@users.noreply.github.com>
2023-08-12 20:49:51 +0700

    cpommit code

---
+++ wwwroot/deploy.ps1
@@ -0,0 +1,38 @@
+﻿# Cấu hình
+$websiteName = "techwiz_btbk"
+$sourceCodePath = "C:\inetpub\wwwroot\techwiz-btbk\be\PlantNest"
+
+# Di chuyển đến thư mục chứa mã nguồn
+Set-Location $sourceCodePath
+
+# Pull mã nguồn mới từ nhánh main
+git pull origin main
+
+# Tắt website trên IIS
+Stop-WebSite -Name $websiteName
+if ($?) {
+    Write-Host "Tắt IIS thành công." -ForegroundColor Green
+} else {
+    Write-Host "Lỗi khi tắt IIS." -ForegroundColor Red
+    exit
+}
+
+# Triển khai ứng dụng
+dotnet publish -c Release -o bin
+if ($?) {
+    Write-Host "Triển khai ứng dụng thành công." -ForegroundColor Green
+} else {
+    Write-Host "Lỗi khi triển khai ứng dụng." -ForegroundColor Red
+    exit
+}
+
+# Bật website trên IIS
+Start-WebSite -Name $websiteName
+if ($?) {
+    Write-Host "Bật IIS thành công." -ForegroundColor Green
+} else {
+    Write-Host "Lỗi khi bật IIS." -ForegroundColor Red
+    exit
+}
+
+# Đợi 3 giây trước khi tắt script
